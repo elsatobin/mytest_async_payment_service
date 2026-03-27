6517df20b15fae340fce743afc0b49c5dfd3515b
elsatobin <elsa0010021@outlook.com>
2026-03-27 04:57:01 +0300

    [D]_unnecessary files and code

--- app/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.db.deps import get_db
-from app.db.session import SessionLocal
-
-__all__ = ["SessionLocal", "get_db"]
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,2 @@
-# app/models/__init__.py
 from .payment import Payment
 from .outbox import Outbox
-
-# Теперь можно импортировать прямо из app.models:
-# from app.models import Payment, Outbox
